751b176021e147c5aaa65d846de63039fe0823c8
Alex Korobov <devasko@ro.ru>
2020-10-23 19:58:48 +0300

    Добавлен контроллер корзины

--- public/js/main.js
+++ public/js/main.js
@@ -1,5 +1,30 @@
 //  Корзина
-$(  );
+$( 'body' ).on( 'click', '.add-to-cart-link', function ( e ) {
+    e.preventDefault();
+    var id = $( this ).data( 'id' ),
+        qty = $( '.quantity input' ).val(),
+        qty = qty ? qty : 1,
+        mod = $( '.available select' ).val();
+
+        $.ajax({
+            url: path + '/cart/add',
+            data: {id: id, qty: qty, mod: mod},
+            type: 'GET',
+
+            success: function ( res ) {
+                showCart( res );
+            },
+
+            error: function () {
+                alert( 'Ошибка! Попробуйте позже' );
+            }
+        });
+
+} );
+
+function showCart( cart ) {
+    console.log( cart );
+}
 
 //  Валюта
 $( '#currency' ).change( function () {
